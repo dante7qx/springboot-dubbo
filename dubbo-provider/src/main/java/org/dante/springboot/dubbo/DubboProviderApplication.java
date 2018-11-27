edce1ef4436a57d6537fe131a785d8484097801a
dante <sunchao.0129@163.com>
2018-11-27 13:37:04 +0800

    修改Provider启动逻辑

--- dubbo-provider/src/main/java/org/dante/springboot/dubbo/DubboProviderApplication.java
+++ dubbo-provider/src/main/java/org/dante/springboot/dubbo/DubboProviderApplication.java
@@ -1,14 +1,18 @@
 package org.dante.springboot.dubbo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 
 /**
- * docker run -d --name dub-provider -p 8801:8801 -e zk_address=192.168.1.4:2181 <Your Image>
- * DUBBO_IP_TO_REGISTRY - 注册到注册中心的IP
- * DUBBO_PORT_TO_REGISTRY - 注册到注册中心的端口
+ * docker run -d --name dub-provider -p 8801:8801 -e zk_address=192.168.1.4:2181
+ * <Your Image> DUBBO_IP_TO_REGISTRY - 注册到注册中心的IP DUBBO_PORT_TO_REGISTRY -
+ * 注册到注册中心的端口
  * 
  * @author dante
  *
@@ -19,15 +23,21 @@ public class DubboProviderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DubboProviderApplication.class, args).start();
-		
-		/*
-		 try {
-			// 按任意键退出
-			System.in.read();
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			String str = br.readLine();
+			while (!"exit".equals(str)) {
+				str = br.readLine();
+			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
-		*/
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
